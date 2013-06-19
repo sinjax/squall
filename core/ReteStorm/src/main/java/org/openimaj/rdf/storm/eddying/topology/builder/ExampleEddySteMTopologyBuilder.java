0d76b7392df11f83e49d4d8236bb9abeba212c6b
David Monks <david.monks@zepler.net>
2013-06-19 16:45:26 +0100

    As no timings are declared as part of the query, setting the age restriction of triples to 1000 minutes (longer than the length of any tests on it)

--- core/ReteStorm/src/main/java/org/openimaj/rdf/storm/eddying/topology/builder/ExampleEddySteMTopologyBuilder.java
+++ core/ReteStorm/src/main/java/org/openimaj/rdf/storm/eddying/topology/builder/ExampleEddySteMTopologyBuilder.java
@@ -35,9 +35,9 @@ import backtype.storm.utils.Utils;
 public class ExampleEddySteMTopologyBuilder extends TopologyBuilder {
 	
 	public static final int SLEEP = 3;
-	public static final int STEMSIZE = 1000;
+	public static final int STEMSIZE = 100;
 	public static final long STEMDELAY = 1000;
-	public static final TimeUnit STEMUNIT = TimeUnit.MILLISECONDS;
+	public static final TimeUnit STEMUNIT = TimeUnit.MINUTES;
 	
 	private String[] subjects = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","28","39"},
 					 predicates = {"pred0",
