b7a59b0ba089150623eba3c819b00de9c088f777
David Monks <david.monks@zepler.net>
2013-08-19 15:21:49 +0100

    added javadoc to EddingBolt interface

--- core/ReteStorm/src/main/java/org/openimaj/rdf/storm/eddying/EddyingBolt.java
+++ core/ReteStorm/src/main/java/org/openimaj/rdf/storm/eddying/EddyingBolt.java
@@ -9,14 +9,31 @@ import backtype.storm.tuple.Values;
  */
 public interface EddyingBolt{
 
+	/**
+	 * @return
+	 * 		The name of the bolt
+	 */
 	public String getName();
 	/**
-	 * Emit the tuple back to the last step of processing.
 	 * @param anchor
+	 * 		The reference tuple to acknowledge
 	 * @param vals
+	 * 		The values in the tuple
 	 */
 	public void emit(Tuple anchor, Values vals);
+	/**
+	 * @param name
+	 * 		Name of the stream to emit to
+	 * @param anchor
+	 * 		The reference tuple to acknowledge
+	 * @param vals
+	 * 		The values in the tuple
+	 */
 	public void emit(String name, Tuple anchor, Values vals);
+	/**
+	 * @param anchor
+	 * 		The reference tuple to acknowledge
+	 */
 	public void ack(Tuple anchor);
 	
 }
