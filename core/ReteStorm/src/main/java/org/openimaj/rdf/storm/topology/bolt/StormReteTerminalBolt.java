fa7e6d73b214f985cf0b3cd0fae73ed12bf1c966
David Monks <david.monks@zepler.net>
2013-10-11 13:43:34 +0100

    Properly combined old ReteTerminal Bolts with StormReteBolt behaviour:
    Original StormReteTerminalBolt: just uses tuple as binding environment, both in and out
    
    Original ReteTerminalBolt: just uses binding environment as value in tuple, both in and out
    
    Needed by ReteConflictSetBolt: binding environment as value in tuple for input
    
    Delivered by StormReteBolts: tuple as binding environment as output
    
    New StormReteTerminalBolt: tuple as binding environment in, binding environment as tuple out.

--- core/ReteStorm/src/main/java/org/openimaj/rdf/storm/topology/bolt/StormReteTerminalBolt.java
+++ core/ReteStorm/src/main/java/org/openimaj/rdf/storm/topology/bolt/StormReteTerminalBolt.java
@@ -33,12 +33,14 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.openimaj.rdf.storm.bolt.RETEStormNode;
+import org.openimaj.rdf.storm.topology.logging.LoggerBolt;
 import org.openimaj.rdf.storm.topology.rules.ReteTopologyRuleContext;
 
+import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
+import backtype.storm.tuple.Fields;
 
-import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.reasoner.rulesys.Rule;
 import com.hp.hpl.jena.reasoner.rulesys.impl.BindingVector;
 import com.hp.hpl.jena.reasoner.rulesys.impl.RETERuleContext;
@@ -77,19 +79,40 @@ public class StormReteTerminalBolt extends StormRuleReteBolt {
 
 	@Override
 	public void execute(Tuple input) {
+//		Rule rule = this.getRule();
+//		Node[] nodes = new Node[rule.getNumVars()];
+//		for (int i = 0; i < nodes.length; i ++)
+//			nodes[i] = (Node) input.getValue(i);
+//		BindingVector env = new BindingVector(nodes);
+//		ReteTopologyRuleContext context = new ReteTopologyRuleContext.IgnoreAdd(rule, env);
+//		logger.debug("Checking rule functors");
+//		if(!context.shouldFire(true)) {
+//			collector.ack(input);
+//			return;
+//		}
+//		Object environment = env.getEnvironment();
+//
+//		Values bindingsRule = new Values(environment,rule.toString());
+//		this.collector.emit(input, bindingsRule);
+//		collector.ack(input);
 		Rule rule = this.getRule();
-		BindingVector env = new BindingVector((Node[]) input.getValues().subList(0, rule.getNumVars()).toArray());
+		BindingVector env = extractBindings(input, rule);
 		ReteTopologyRuleContext context = new ReteTopologyRuleContext.IgnoreAdd(rule, env);
 		logger.debug("Checking rule functors");
 		if(!context.shouldFire(true)) {
 			collector.ack(input);
 			return;
 		}
-		Object environment = env.getEnvironment();
 
-		Values bindingsRule = new Values(environment,rule.toString());
+		Values bindingsRule = new Values(env.getEnvironment(),rule.toString());
 		this.collector.emit(input, bindingsRule);
 		collector.ack(input);
+	}
+	
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("bindings", "rule"));
+		LoggerBolt.LogEmitter.declareFields(declarer);
 	}
 
 	@Override
