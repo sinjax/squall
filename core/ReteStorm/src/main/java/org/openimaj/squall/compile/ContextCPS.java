80c2eb32deed180e5c71363fc91ee951281ee0aa
David Monks <david.monks@zepler.net>
2013-10-17 15:02:19 +0100

    removed cloning from ContextCPS

--- core/ReteStorm/src/main/java/org/openimaj/squall/compile/ContextCPS.java
+++ core/ReteStorm/src/main/java/org/openimaj/squall/compile/ContextCPS.java
@@ -1,40 +1,10 @@
 package org.openimaj.squall.compile;
 
-import java.util.List;
-
-import org.openimaj.squall.compile.data.IStream;
-import org.openimaj.squall.compile.data.IVFunction;
-import org.openimaj.util.data.Context;
-
 
 /**
- * @author Sina Samangooei (ss@ecs.soton.ac.uk), David Monks <dm11g08@ecs.soton.ac.uk>
+ * @author Sina Samangooei (ss@ecs.soton.ac.uk)
  *
  */
 public class ContextCPS extends CompiledProductionSystem{
-
-	@Override
-	public ContextCPS clone() {
-		ContextCPS clone = new ContextCPS();
-		
-		for (IStream<Context> source : this.sources){
-			clone.addSource(source);
-		}
-		for (CompiledProductionSystem cps : this.systems){
-				clone.addSystem(cps.clone());
-		}
-		for (JoinComponent<?> func : this.joinlist){
-			clone.addJoinComponent(func);
-		}
-		for (IVFunction<Context,Context> func : this.predicates){
-			clone.addPredicate(func);
-		}
-		for (IVFunction<List<Context>,Context> func : this.aggregations){
-			// TODO aggregations
-		}
-		clone.setConsequence(this.consequence);
-		
-		return clone;
-	}
 
 }
