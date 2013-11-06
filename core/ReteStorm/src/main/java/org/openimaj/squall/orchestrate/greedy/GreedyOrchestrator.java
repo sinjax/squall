bc964508b5a8a0caa2570529c3f2e41c3a0dfd48
Sina Samangooei <sinjax@gmail.com>
2013-11-06 15:36:03 +0000

    fixed but still broken, becuase i think the compiler is broken. WE NEED TO TALK DAVID

--- core/ReteStorm/src/main/java/org/openimaj/squall/orchestrate/greedy/GreedyOrchestrator.java
+++ core/ReteStorm/src/main/java/org/openimaj/squall/orchestrate/greedy/GreedyOrchestrator.java
@@ -103,9 +103,7 @@ public class GreedyOrchestrator implements Orchestrator{
 		List<NamedNode<? extends IVFunction<Context, Context>>> joinedCPS = new ArrayList<NamedNode<? extends IVFunction<Context, Context>>>();
 		for (CompiledProductionSystem cps : sys.getSystems()) {
 			NamedNode<? extends IVFunction<Context, Context>> combined = orchestrate(root,cps);
-			if(combined == null){
-				throw new RuntimeException("No consequence of or'ed "); 
-			}
+			
 			if(combinedFilters != null){ // join the sub systems to any filters
 				combined = createJoinNode(root, combined,combinedFilters);
 			}
@@ -116,7 +114,10 @@ public class GreedyOrchestrator implements Orchestrator{
 			joinedCPS.add(combinedFilters);
 		}
 //		aggregations = orchestrateAggregations(joinedCPS,sys.getAggregations());
-		if(sys.getConsequence() == null){ return null; }
+		if(sys.getConsequence() == null){
+			// Use an implicity consequence that is a simple passthrough function
+			return orchestrateConsequences(root, joinedCPS,new PassThroughConsequence()); 
+		}
 		return orchestrateConsequences(root, joinedCPS,sys.getConsequence());
 	}
 
