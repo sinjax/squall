6fdaccfe76686e75ab76b13e872179e67660bd53
David Monks <david.monks@zepler.net>
2013-11-26 16:54:20 +0000

    Moved the Sandbox testing about, moved large triples files to the sandbox lsbench data folder, but ignore them (wherever they are placed)

---
+++ sandbox/src/test/java/org/openimaj/squall/compile/rif/TestRifRuleCompilerCombinedSourceGreedyOrchestratorStormBuilder.java
@@ -0,0 +1,158 @@
+package org.openimaj.squall.compile.rif;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openimaj.rif.RIFRuleSet;
+import org.openimaj.rif.conditions.atomic.RIFAtom;
+import org.openimaj.rif.conditions.data.RIFExternalExpr;
+import org.openimaj.rif.conditions.formula.RIFExternalValue;
+import org.openimaj.rif.imports.profiles.RIFEntailmentImportProfiles;
+import org.openimaj.squall.build.Builder;
+import org.openimaj.squall.build.storm.StormStreamBuilder;
+import org.openimaj.squall.compile.CompiledProductionSystem;
+import org.openimaj.squall.compile.data.IOperation;
+import org.openimaj.squall.compile.data.IVFunction;
+import org.openimaj.squall.compile.functions.rif.external.ExternalLoader;
+import org.openimaj.squall.compile.functions.rif.predicates.NumericRIFPredicateFunction;
+import org.openimaj.squall.compile.rif.provider.ExternalFunctionProvider;
+import org.openimaj.squall.compile.rif.provider.ExternalFunctionRegistry;
+import org.openimaj.squall.functions.rif.predicates.BaseRIFPredicateFunction.RIFPredicateException;
+import org.openimaj.squall.orchestrate.OrchestratedProductionSystem;
+import org.openimaj.squall.orchestrate.greedy.CombinedSourceGreedyOrchestrator;
+import org.openimaj.squall.orchestrate.greedy.GreedyOrchestrator;
+import org.openimaj.util.data.Context;
+import org.xml.sax.SAXException;
+
+import com.hp.hpl.jena.graph.Node;
+
+/**
+ * @author Sina Samangooei (ss@ecs.soton.ac.uk)
+ *
+ */
+public class TestRifRuleCompilerCombinedSourceGreedyOrchestratorStormBuilder {
+	
+	private static final class PrintAllOperation implements IOperation<Context>, Serializable {
+		@Override
+		public void setup() {
+			System.out.println("Starting Test");
+		}
+
+		@Override
+		public void cleanup() {
+		}
+
+		@Override
+		public void perform(Context object) {
+			String typed = object.getTyped("rule");
+			if (typed != null && typed.endsWith("lsbench-query-7.5")) 
+				System.out.println(object);
+		}
+	}
+
+
+
+	private RIFRuleSet simpleRules;
+	private RIFRuleSet simplejoinRules;
+	private RIFRuleSet complexjoinRules;
+	private RIFRuleSet multiunionRules;
+	private RIFRuleSet lsbench;
+
+	private RIFRuleSet readRules(String ruleSource) {
+		RIFRuleSet rules = null;
+		RIFEntailmentImportProfiles profs = new RIFEntailmentImportProfiles();
+		try {
+			InputStream resourceAsStream = TestRifRuleCompilerCombinedSourceGreedyOrchestratorStormBuilder.class.getResourceAsStream(ruleSource);
+//			System.out.println(FileUtils.readall(resourceAsStream));
+			rules = profs.parse(
+					resourceAsStream,
+					new URI("http://www.w3.org/ns/entailment/Core")
+				);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return rules;
+	}
+	/**
+	 * @throws IOException 
+	 * 
+	 */
+	@Before
+	public void before() throws IOException{
+		this.simpleRules = readRules("/test.simple.rule.rif");
+		this.simplejoinRules = readRules("/test.simplejoin.rule.rif");
+		this.complexjoinRules = readRules("/test.complexjoin.rule.rif");
+		this.multiunionRules = readRules("/test.multiunion.rule.rif");
+		this.lsbench = readRules("/lsbench/queries/queries.rif");
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testSimpleRulesBuilder(){
+		testRuleSet(simpleRules);
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testSimpleJoinBuilder(){
+		testRuleSet(simplejoinRules);
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testComplexRules(){
+		testRuleSet(complexjoinRules);
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testMultiUnionRules(){
+		testRuleSet(multiunionRules);
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testLSBenchRulesBuilder(){
+		testRuleSet(lsbench);
+	}
+	
+	private void testRuleSet(RIFRuleSet ruleSet) {
+		ExternalLoader.loadExternals();
+		
+		IOperation<Context> op = new PrintAllOperation();
+
+		RIFCoreRuleCompiler jrc = new RIFCoreRuleCompiler();
+		CompiledProductionSystem comp = jrc.compile(ruleSet);
+		
+		CombinedSourceGreedyOrchestrator go = new CombinedSourceGreedyOrchestrator();
+		OrchestratedProductionSystem orchestrated = go.orchestrate(comp, op );
+		
+		Builder builder = StormStreamBuilder.localClusterBuilder(-1);
+		builder.build(orchestrated);
+	}
+	
+
+}
